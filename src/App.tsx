c13627d80e9455e63aba91318b3a60e31888aaa3
Arsia Ardalan <aardalan@rossvideo.com>
2022-02-16 00:06:34 -0500

    feat: removes files to introduce new ones later

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+
+const App = (props)=>{
+    console.log('logging from App')
+
+    return (
+        <>
+        <h1>this is header in App component</h1>
+        <h4>This project goes through React's official documentation found here "https://reactjs.org/" in order to learn react</h4>
+        </>
+    )
+}
+
+export default App;
